1d363d621fe8b27422be47bee4aeb408825fb34f
Renato Akio Hossotani <renato.hossotani@indigosoft.com.br>
2023-07-26 11:23:10 -0300

    Correção token/refresh token

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
     secret_token: "9755c734bd7fbf82f710a3a03fc71c22",
-    expires_in: "15m",
+    expires_in: "30s",
     secret_refresh_token: "7aa3930984df98ca60d43f1f89eaba9c",
     expires_in_refresh_token: "30d",
     expires_refresh_token_days:30
